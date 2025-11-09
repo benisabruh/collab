6cf7418f6c211fd3c81b0d1fe536e1e15ad4b6b9
benisabruh <bencarterfox@yahoo.com>
2025-11-08 21:32:24 -0500

    ben

--- Assets/Scripts/BackpackCollector.cs
+++ Assets/Scripts/BackpackCollector.cs
@@ -4,19 +4,33 @@ public class BackpackCollector : MonoBehaviour
 {
     [SerializeField]
     private GameObject houseDoorCollider; 
+    [SerializeField]
+    private AudioSource equipAudioSource;
+    [SerializeField]
+    private AudioClip equipClip;
+
+ 
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("backpack"))
         {
-            Debug.Log("ahhh");
-            // Make the backpack disappear
+            Debug.Log("Backpack");
+
+
+            if (equipAudioSource != null)
+                equipAudioSource.PlayOneShot(equipClip);
+            else
+                AudioSource.PlayClipAtPoint(equipClip, transform.position);
+
             other.gameObject.SetActive(false);
-            
+
             // Enable the house door collider
             if (houseDoorCollider != null)
             {
                 houseDoorCollider.SetActive(true);
+               
             }
            
         }
