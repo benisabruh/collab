171f0f6c4f2a61f35f58c4c70f1c3796e0fe5377
benisabruh <bencarterfox@yahoo.com>
2025-11-10 17:18:35 -0500

    audio fixes

--- Assets/Scripts/CollideTrigger.cs
+++ Assets/Scripts/CollideTrigger.cs
@@ -9,21 +9,37 @@ using UnityEngine.SceneManagement;
 public class CollideTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject restart; 
+    [SerializeField] private AudioSource equipAudioSource;
+    
+    [SerializeField] private AudioClip equipClip;
     private void OnTriggerEnter(Collider other)
     {
         // Check if collision is made with the Drop, which is tagged as "Finish"
         // You can change the tag and manually change this accordingly.
         if (other.tag == "busDoor")
         {
+            if (equipAudioSource != null)
+                equipAudioSource.PlayOneShot(equipClip);
+            else
+                AudioSource.PlayClipAtPoint(equipClip, transform.position);
             SceneManager.LoadScene("insideBus");
+            
         }
         if (other.tag == "houseDoor")
         {
+            if (equipAudioSource != null)
+                equipAudioSource.PlayOneShot(equipClip);
+            else
+                AudioSource.PlayClipAtPoint(equipClip, transform.position);
             SceneManager.LoadScene("Outside");
         }
 
         if (other.tag == "bus2school")
         {
+            if (equipAudioSource != null)
+                equipAudioSource.PlayOneShot(equipClip);
+            else
+                AudioSource.PlayClipAtPoint(equipClip, transform.position);
             SceneManager.LoadScene("classroomScene");
         }
         if (other.tag == "classroomDoor")
